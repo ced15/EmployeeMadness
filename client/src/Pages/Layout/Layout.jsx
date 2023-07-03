c5a3c751415863527d428becf4ce8437a0ce3e09
Denisa Cuta <denisa1506@yahoo.com>
2023-07-03 12:02:42 +0300

    equipment

--- client/src/Pages/Layout/Layout.jsx
+++ client/src/Pages/Layout/Layout.jsx
@@ -1,23 +1,35 @@
-import { Outlet, Link } from "react-router-dom";
-
+import { Outlet, Link, useLocation } from "react-router-dom";
 import "./Layout.css";
 
-const Layout = () => (
-  <div className="Layout">
-    <nav>
-      <ul>
-        <li className="grow">
-          <Link to="/">Employees</Link>
-        </li>
-        <li>
-          <Link to="/create">
-            <button type="button">Create Employee</button>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <Outlet />
-  </div>
-);
+const Layout = () => {
+  const location = useLocation();
+  const isEquipmentPage = location.pathname === "/equipment";
+
+  return (
+    <div className="Layout">
+      <nav>
+        <ul>
+          <li className="grow">
+            <Link to="/">Employees</Link>
+          </li>
+          {isEquipmentPage ? (
+            <li>
+              <Link to="/createequipment">
+                <button type="button">Create Equipment</button>
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/create">
+                <button type="button">Create Employee</button>
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
 
 export default Layout;
